8ee0c1403c351a4df7ce2f13876f0c76f1eef24f
tony831993 <tanmay831993@gmail.com>
2024-01-18 22:30:39 +0530

    Add bcrypt for password hashing and jwt authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Thisisasample@JWTSecret';
 
 // Create a user using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -30,15 +34,24 @@ router.post('/createuser', [
         // Check for existing user email in DB.
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: 'User with given email already exists.' });
+            return res.status(400).json({ error: `User with given email "${req.body.email}" already exists.` });
         }
+        const salt = await bcrypt.genSaltSync(10);
+        const securePass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: securePass
         });
         if (user) {
-            res.json({ success: `User "${user.email}" created successfuly.` });
+            const userData = {
+                user: user.id
+            };
+            const authToken = jwt.sign(userData, JWT_SECRET);
+            res.json({
+                success: `User "${user.email}" created successfuly.`,
+                authToken
+            });
         } else {
             res.status(500).json({ error: `Unable to create the user. Please try again later.` });
         }
